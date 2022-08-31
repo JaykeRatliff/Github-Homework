a878149a92c02891045f4b04f66224b7ef4a736e
JaykeRatliff <112008581+JaykeRatliff@users.noreply.github.com>
2022-08-31 18:29:09 -0500

    Add files via upload

---
+++ Github 1.py
@@ -0,0 +1,40 @@
+### Python Exercises: Variables (3 pts.)
+hometown = 'Stillwater'
+# Place other variables here
+x=298784
+j=5
+l=10
+z=j+l
+t=q=r=s='education'
+### Python Exercises: Data Types (2 pts.)
+def data_types():
+    a = ["apple", "banana", "cherry"]
+    print(type(a)) 
+    b = ("apple", "banana", "cherry")
+    print(type(a))
+    c = False
+    print(type(c))
+    d = 'covid-19'
+    print(type(d))
+    e = int(487)
+
+data_types()
+
+### Python Exercises: Data Types 2 (3 pts.)
+def data_types2():
+    a = ["apple", "banana", "cherry"]
+    b = ("apple", "banana", "cherry")
+    c = False
+    d = 'covid-19'
+    e = int(487)
+    print('a','b,''d')
+
+data_types2()
+
+### Python Exercises: Iteration (2 pts.)
+def food_loop():
+    lunches = ['burger','apple','soba','chahan']
+    for x in lunches:
+        print(x)
+
+food_loop()
